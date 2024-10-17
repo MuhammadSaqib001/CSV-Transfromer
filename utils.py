c23d4f60358bba5086158e8d41fdb6f90e529dbe
Muhammad Saqib <saqib@monetizemore.com>
2024-10-17 23:06:58 +0500

    required updates by client

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # Function to process the CSV data before displaying and before downloading
-def process_csv_data(df):
-    # 1. Remove the column "Custom text"
+def process_csv_data(df): 
+    # 1. Set all values in "Custom text" to an empty string instead of removing the column
     if 'Custom text' in df.columns:
-        df.drop(columns=['Custom text'], inplace=True)
+        df['Custom text'] = ''
 
     # 2. Remove quotation marks from strings in the DataFrame
     df = df.applymap(lambda x: x.replace('"', '') if isinstance(x, str) else x)
@@ -26,10 +26,20 @@ def process_csv_data(df):
             'Next Day - Tracked 24': 'TPN24'
         }).fillna('')  # Fill NaN values with an empty string
 
-
+    # 6. Rename 'Delivery zip/postal code' to 'Delivery ZIP/postal code' if it exists
     if 'Delivery zip/postal code' in df.columns:
         df.rename(columns={'Delivery zip/postal code': 'Delivery ZIP/postal code'}, inplace=True)
 
+    # 7. Reorder columns
+    desired_order = [
+        'Order number', 'Date created', 'Total order quantity', 'Contact email', 'Note from customer',
+        'Item', 'Variant', 'SKU', 'Qty', 'Quantity refunded', 'Price', 'Weight', 'Custom text',
+        'Deposit amount', 'Delivery method', 'Recipient name', 'Delivery country', 'Delivery city',
+        'Delivery address', 'Delivery ZIP/postal code', 'Service Code', 'Parcel Size'
+    ]
+
+    # Ensure all columns in desired order are in the DataFrame before reordering
+    df = df.reindex(columns=desired_order)
     return df
 
 def adjust_all_columns_to_string(df):
